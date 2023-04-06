d0f2ec183550eb9fcddbdee1494f57779f651861
Aaron <aarocka@protonmail.com>
2023-04-06 15:03:18 -0400

    commit!

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -17,15 +17,32 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.viewPager)
     }
 
+    private var numberOfPages = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewPager.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numberOfPages
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
 
+
+
+        }
+        //create new fragment
+        newButton.setOnClickListener {
+            numberOfPages++
+
+
+            //viewPager.adapter?.notifyDataSetChanged()
+
+            viewPager.adapter?.notifyItemInserted(numberOfPages - 1)
         }
 
     }
